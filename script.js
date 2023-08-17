a8284deb76350590b15c72cf6144b8e287393e1c
Erica Ayala <erica.ayala.work@gmail.com>
2023-08-16 22:08:45 -0400

    Rewrite functions and change  logic in JavaScript file. Page now displays Xs in all 3 rows, just needs to display the Os as well.

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-let h2 = document.querySelector("h2");
-
-const boxes = document.querySelectorAll(".boxes");
-
 const topLeftValue = document.querySelector("#top-left").innerHTML;
 const topMiddleValue = document.querySelector("#top-middle").innerHTML;
 const topRightValue = document.querySelector("#top-right").innerHTML;
@@ -12,78 +8,71 @@ const bottomLeftValue = document.querySelector("#bottom-left").innerHTML;
 const bottomMiddleValue = document.querySelector("#bottom-middle").innerHTML;
 const bottomRightValue = document.querySelector("#bottom-right").innerHTML;
 
-let player1Score = document.querySelector("#player1-score").innerHTML;
-let tieCounter = document.querySelector("#tie-counter").innerHTML;
-let player2Score = document.querySelector("#player2-score").innerHTML;
+let player1Score = parseInt(document.querySelector("#player1-score").innerHTML);
+let tieCounter = parseInt(document.querySelector("#tie-counter").innerHTML);
+let player2Score = parseInt(document.querySelector("#player2-score").innerHTML);
 
-const button = document.querySelector("button");
+let boxes = document.querySelectorAll(".boxes");
+let h2 = document.querySelector("h2");
+let button = document.querySelector("button");
 
+function playGame(event) {
+    
+    let h2 = document.querySelector("h2");
+    let currentPlayer = "Player 1";
+    let clickedBox = event.target;
 
-let currentPlayer = "Player 1";
+    if (currentPlayer === "Player 1" && (clickedBox.innerHTML !== "X" || clickedBox.innerHTML !== "O")) {
+            
+        clickedBox.innerHTML = "X";
 
-function switchPlayers() {
-    boxes.forEach(box => {
-        if (box.innerHTML === "" && currentPlayer === "Player 1") {
-            box.innerHTML = currentPlayer;
-            currentPlayer = "Player 2";
-        } else if (box.innerHTML === "" && currentPlayer === "Player 2") {
-            box.innerHTML = currentPlayer;
-            currentPlayer = "Player 1";
-        }
-    })
-}
-
-
-function displayPlayerChoice(box) {
-    if (currentPlayer === "Player 1") {
-        box.innerHTML = "X";
-        
         if ((topLeftValue === "X" && topMiddleValue === "X" && topRightValue === "X") || (middleLeftValue === "X" && centerValue === "X" && middleRightValue === "X") || (bottomLeftValue === "X" && bottomMiddleValue === "X" && bottomRightValue === "X") || (topLeftValue === "X" && centerValue === "X" && bottomRightValue === "X") || (topRightValue === "X" && centerValue === "X" && bottomLeftValue === "X")) {
+                
             h2.innerHTML = "Player 1 wins!";
+            player1Score += 1;
+                
+        } else {
+            currentPlayer = "Player 2";
+            h2.innerHTML = "Player 2's turn!";
+        }
 
-        } else if ((topLeftValue === "O" && topMiddleValue === "X" && topRightValue === "O") || (middleLeftValue === "O" && centerValue === "O" && middleRightValue === "O") || (bottomLeftValue === "O" && bottomMiddleValue === "O" && bottomRightValue === "O") || (topLeftValue === "O" && centerValue === "O" && bottomRightValue === "X") || (topRightValue === "O" && centerValue === "O" && bottomLeftValue === "O")) {
-            h2.innerHTML = "Player 2 wins!"
+    } else if (currentPlayer === "Player 2" && (clickedBox.innerHTML !== "X" || clickedBox.innerHTML !== "O")) {
+            
+        clickedBox.innerHTML = "O";
+
+        if ((topLeftValue === "O" && topMiddleValue === "X" && topRightValue === "O") || (middleLeftValue === "O" && centerValue === "O" && middleRightValue === "O") || (bottomLeftValue === "O" && bottomMiddleValue === "O" && bottomRightValue === "O") || (topLeftValue === "O" && centerValue === "O" && bottomRightValue === "X") || (topRightValue === "O" && centerValue === "O" && bottomLeftValue === "O")) {
+
+            h2.innerHTML = "Player 2 wins!";
+            player2Score += 1;
 
         } else {
-            h2.innerHTML = "It's a tie!";
+            currentPlayer = "Player 1";
+            h2.innerHTML = "Player 1's turn!";
         }
 
     } else {
-        box.innerHTML = "O";
+        h2.innerHTML = "It's a tie!";
+        tieCounter += 1;
     }
-}
 
-
-function updateScores() {
-    if (h2.innerHTML === "Player 1 wins!") {
-        player1Score = parseInt(player1Score.innerHTML) + 1;
-    } else if (h2.innerHTML === "Player 2 wins!") {
-        player2Score = parseInt(player2Score.innerHTML) + 1;
-    } else if (h2.innerHTML === "It's a tie!") {
-        tieCounter = parseInt(tieCounter.innerHTML) + 1;
-    }
-}
-
-
-function updateCurrentPlayerDisplay() {
     
 }
 
-function playGame() {
-    boxes.forEach(box => {
-        box.addEventListener("click", () => {
-            if (box.innerHTML === "") {
-                displayPlayerChoice(box);
-                switchPlayers();
-                updateScores();
-                updateCurrentPlayerDisplay();
-            }
-        })
-    })
+
+for (let i = 0; i < boxes.length; i++) {
+        boxes[i].addEventListener("click", playGame);
 }
 
 
 function playAgain(event) {
     event.preventDefault();
-    playGame();
-} "
+
+    boxes.forEach(box => {
+        box.innerHTML = "";
+    })
+
+    h2.innerHTML = "";
+    currentPlayer = "Player 1";
+}
+
+button.addEventListener("click", playAgain);
