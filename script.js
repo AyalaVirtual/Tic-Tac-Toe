baac3d6916fc4dc78e35ff69345a19d3c4fc2846
Erica Ayala <erica.ayala.work@gmail.com>
2023-08-17 15:59:47 -0400

    Change querySelectors for square variables by removing .innerHTML and adding .innerText to winning conditions. Shorten variable names for squares. Game recognizes when player wins. Still not updating scores.

--- script.js
+++ script.js
@@ -13,23 +13,25 @@ function playGame(event) {
     let clickedBox = event.target;
 
     // Fetch the values of the squares 
-    const topLeftValue = document.querySelector("#top-left").innerHTML;
-    const topMiddleValue = document.querySelector("#top-middle").innerHTML;
-    const topRightValue = document.querySelector("#top-right").innerHTML;
-    const middleLeftValue = document.querySelector("#middle-left").innerHTML;
-    const centerValue = document.querySelector("#center").innerHTML;
-    const middleRightValue = document.querySelector("#middle-right").innerHTML;
-    const bottomLeftValue = document.querySelector("#bottom-left").innerHTML;
-    const bottomMiddleValue = document.querySelector("#bottom-middle").innerHTML;
-    const bottomRightValue = document.querySelector("#bottom-right").innerHTML;
+    const topLeft = document.querySelector("#top-left");
+    const topMiddle = document.querySelector("#top-middle");
+    const topRight = document.querySelector("#top-right");
+    const middleLeft = document.querySelector("#middle-left");
+    const center = document.querySelector("#center");
+    const middleRight = document.querySelector("#middle-right");
+    const bottomLeft = document.querySelector("#bottom-left");
+    const bottomMiddle = document.querySelector("#bottom-middle");
+    const bottomRight = document.querySelector("#bottom-right");
 
 
     if (clickedBox.innerHTML === "") {
         if (currentPlayer === "Player 1") {
             clickedBox.innerHTML = "X";
             
+            // console.log(topLeft.innerText);
+
             // Check win conditions for Player 1
-            if ((topLeftValue === "X" && topMiddleValue === "X" && topRightValue === "X") || (middleLeftValue === "X" && centerValue === "X" && middleRightValue === "X") || (bottomLeftValue === "X" && bottomMiddleValue === "X" && bottomRightValue === "X") || (topLeftValue === "X" && centerValue === "X" && bottomRightValue === "X") || (topRightValue === "X" && centerValue === "X" && bottomLeftValue === "X")) {
+            if ((topLeft.innerText === "X" && topMiddle.innerText === "X" && topRight.innerText === "X") || (middleLeft.innerText === "X" && center.innerText === "X" && middleRight.innerText === "X") || (bottomLeft.innerText === "X" && bottomMiddle.innerText === "X" && bottomRight.innerText === "X") || (topLeft.innerText === "X" && center.innerText === "X" && bottomRight.innerText === "X") || (topRight.innerText === "X" && center.innerText === "X" && bottomLeft.innerText === "X") || (topLeft.innerText === "X" && middleLeft.innerText === "X" && bottomLeft.innerText === "X") || (topMiddle.innerText === "X" && center.innerText === "X" && bottomMiddle.innerText === "X") || (topRight.innerText === "X" && middleRight.innerText === "X" && bottomRight.innerText === "X")) {
 
                 h2.innerHTML = "Player 1 wins!";
                 player1Score += 1;
@@ -43,7 +45,7 @@ function playGame(event) {
             clickedBox.innerHTML = "O";
 
             // Check win conditions for Player 2
-            if ((topLeftValue === "O" && topMiddleValue === "O" && topRightValue === "O") || (middleLeftValue === "O" && centerValue === "O" && middleRightValue === "O") || (bottomLeftValue === "O" && bottomMiddleValue === "O" && bottomRightValue === "O") || (topLeftValue === "O" && centerValue === "O" && bottomRightValue === "O") || (topRightValue === "O" && centerValue === "O" && bottomLeftValue === "O")) {
+            if ((topLeft.innerText === "O" && topMiddle.innerText === "O" && topRight.innerText === "O") || (middleLeft.innerText === "O" && center.innerText === "O" && middleRight.innerText === "O") || (bottomLeft.innerText === "O" && bottomMiddle.innerText === "O" && bottomRight.innerText === "O") || (topLeft.innerText === "O" && center.innerText === "O" && bottomRight.innerText === "O") || (topRight.innerText === "O" && center.innerText === "O" && bottomLeft.innerText === "O") || (topLeft.innerText === "O" && middleLeft.innerText === "O" && bottomLeft.innerText === "O") || (topMiddle.innerText === "O" && center.innerText === "O" && bottomMiddle.innerText === "O") || (topRight.innerText === "O" && middleRight.innerText === "O" && bottomRight.innerText === "O")) {
                 
                 h2.innerHTML = "Player 2 wins!";
                 player2Score += 1;
